d09b7e9424c8e3d3e14e8f9437718fe8eba00185
JayShu <18611525826@163.com>
2017-02-28 09:06:46 +0800

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -9,29 +9,29 @@ public class Bishop implements Pieces
 {
     public String name ="Bishop";
     public int x,y;
-    public String GetName(){
+    public String getName(){
         return name;
     }
-    public void SetX(int ix){
+    public void setX(int ix){
         x = ix;
     }
-    public void SetY(int iy){
+    public void setY(int iy){
         y = iy;
     }
-    public Bishop(int ix,int iy){
+    public bishop(int ix,int iy){
         x = ix; y = iy;
     }
-    public int GetX(){
+    public int getX(){
         return x;
     }
-    public int GetY(){
+    public int getY(){
         return y;
     }
     public Boolean isAbleToMove(int tox,int toy){
         return false;
     }
-    public void Move(int tox,int toy){
-        x = tox;
-        y = toy;
+    public void move(int toX,int toY){
+        x = toX;
+        y = toY;
     }
 }
