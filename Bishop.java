f9011f08ee9ba3be8a3da1197dad8d36698d20e7
EdmundWu <wuyuxuan_awu@163.com>
2017-03-02 12:56:00 +0800

    Add files via upload

--- Bishop.java
+++ Bishop.java
@@ -1,4 +1,4 @@
-
+package chessGame;
 /**
  * Write a description of class Chess_Bishop here.
  * 
@@ -29,10 +29,97 @@ public class Bishop implements Pieces
         return y;
     }
     public Boolean isAbleTomove(int tox,int toy){
-        return false;
+    	//Pieces[][] i = Board.ba;
+        boolean decide = true;
+        boolean RU = false;
+        boolean LU = false;
+        boolean RD = false;
+        boolean LD = false;
+        //Board.ba[3][4].GetName
+        if(Math.abs(tox-x)==Math.abs(toy-y)){
+            if(tox>x){
+                if(toy>y){
+                    RU = true;
+                }
+                else{
+                    RD = true;
+                }
+            }
+            else if(tox<x){
+                if(toy>y){
+                    LU = true;
+                }
+                else{
+                    LD = true;
+                }
+            }
+            if(RU){
+                decide = RU(tox,toy);
+            }
+            else if(RD){
+                decide = RD(tox,toy);
+            }
+            else if(LU){
+                decide = LU(tox,toy);
+            }
+            else if(LD){
+                decide = LD(tox,toy);
+            }
+        }
+        else{
+            decide = false;
+        }
+        return decide;
     }
     public void move(int tox,int toy){
         x = tox;
         y = toy;
+    }
+    public boolean RU(int tox,int toy){
+        boolean decideRU = true;
+        for(int i=x;i<tox;i++){
+            for(int j=y;j<toy;j++){
+                if(Board.ba[i][j].getName()!="   "){
+                    decideRU = false;
+                }
+            }
+        }
+        return decideRU;
+    }
+
+    public boolean RD(int tox,int toy){
+        boolean decideRD = true;
+        for(int i=x;i<tox;i++){
+            for(int j=y;j>toy;j--){
+                if(Board.ba[i][j].getName()!="   "){
+                    decideRD = false;
+                }
+            }
+        }
+        return decideRD;
+    } 
+
+    public boolean LU(int tox,int toy){
+        boolean decideLU = true;
+        for(int i=x;i>tox;i--){
+            for(int j=y;j<toy;j++){
+                if(Board.ba[i][j].getName()!="   "){
+                    decideLU = false;
+                }
+            }
+        }
+        return decideLU;
+    }
+
+    public boolean LD(int tox,int toy){
+        boolean decideLD = true;
+        for(int i=x;i>tox;i--){
+            for(int j=y;j>toy;j--){
+                if(Board.ba[i][j].getName()!="   "){
+                    decideLD = false;
+                }
+            }
+        }
+        return decideLD;
     }
 }
