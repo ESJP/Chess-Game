cb2616e1e8f83ed99ed69160c0dc8aafdff11674
EdmundWu <wuyuxuan_awu@163.com>
2017-03-09 13:00:59 +0800

    Add files via upload

--- King.java
+++ King.java
@@ -31,22 +31,23 @@ public class King implements Pieces
     }
     public Boolean isAbleToMove(int tox,int toy){
     	boolean decide = true;
-    	String newLocation = "";
+    	/*String newLocation = "";
     	String nowLocation = "";
     	//String sideNow = "";
     	//String sideNew = "";
-    	if(Math.abs(tox-x)==1 && Math.abs(toy-y)==1){
-    	  if(Board.ba[tox][toy].getName()!="   "){
-    		newLocation=Board.ba[tox][toy].getName();
-    		nowLocation=Board.ba[x][y].getName();
+    	if(Math.abs(tox-x)<=1 && Math.abs(toy-y)<=1){
+    	  if(Board.ba[tox-1][toy-1].getName()!="   "){
+    		//newLocation=Board.ba[tox][toy].getName();
+    		//nowLocation=Board.ba[x][y].getName();
     		if( nowLocation.substring(0,1)==newLocation.substring(0,1))decide = false;
     	}
     	}
     	else{
     		decide = false;
     	}
-    	
+    	*/
     	return decide;
+    	
     	}
        
     
