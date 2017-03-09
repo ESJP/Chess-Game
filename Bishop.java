90f34ee89e7daadd89ec7d2bc31b083a7cc5948f
EdmundWu <wuyuxuan_awu@163.com>
2017-03-09 12:55:19 +0800

    Add files via upload

--- Bishop.java
+++ Bishop.java
@@ -30,7 +30,7 @@ public class Bishop implements Pieces
     }
     public Boolean isAbleToMove(int tox,int toy){
     	//Pieces[][] i = Board.ba;
-        boolean decide = true;
+        boolean decide = false;
         boolean rightUp = false;
         boolean leftUp = false;
         boolean rightDown = false;
@@ -69,57 +69,89 @@ public class Bishop implements Pieces
         else{
             decide = false;
         }
+        System.out.println(Board.ba[1][1].getName());
+        //System.out.println("1");
+        System.out.println(Board.ba[3][1].getName());
         return decide;
-    }
+        }
+    
+    
     public void move(int tox,int toy){
         x = tox;
         y = toy;
     }
     public boolean rightUp(int tox,int toy){
-        boolean decideRightUp = true;
-        for(int i=x;i<tox;i++){
-            for(int j=y;j<toy;j++){
-                if(!Board.ba[i][j].getName().equals("   ")){
-                    decideRightUp = false;
+        boolean decideRightUp = false;
+        int count = 0;
+        int j = y;
+        for(int i = x;i<tox;i++){
+            j++;
+            //System.out.println(Board.ba[4][2].getName());
+                if(!Board.ba[i-1][j-1].getName().equals("   ")){
+                	//System.out.println(Board.ba[i-1][j-1].getName());
+                    //System.out.println("1");
+                    count = count +1;
                 }
-            }
+            
+        }
+        if(count == 0){
+        	decideRightUp = true;
         }
         return decideRightUp;
-    }
+        
+        }
+    
 
     public boolean rightDown(int tox,int toy){
-        boolean decideRightDown = true;
+        boolean decideRightDown = false;
+        int count = 0;
+        int j = y;
         for(int i=x;i<tox;i++){
-            for(int j=y;j>toy;j--){
-                if(!Board.ba[i][j].getName().equals("   ")){
-                    decideRightDown = false;
+            j--;
+                if(!Board.ba[i-1][j-1].getName().equals("   ")){
+                	count ++;
                 }
-            }
+            
+        }
+        if(count == 0){
+        	decideRightDown = true;
         }
         return decideRightDown;
     } 
 
     public boolean leftUp(int tox,int toy){
-        boolean decideLeftUp = true;
+        boolean decideLeftUp = false;
+        int count = 0;
+        int j = y;
         for(int i=x;i>tox;i--){
-            for(int j=y;j<toy;j++){
-                if(!Board.ba[i][j].getName().equals("   ")){
-                    decideLeftUp = false;
+            j++;
+                if(!Board.ba[i-1][j-1].getName().equals("   ")){
+                	count ++;
                 }
-            }
+            
+        }
+        if(count == 0){
+        	decideLeftUp = true;
         }
         return decideLeftUp;
     }
 
     public boolean leftDown(int tox,int toy){
         boolean decideLeftDown = true;
+        int count = 0;
+        int j = y;
         for(int i=x;i>tox;i--){
-            for(int j=y;j>toy;j--){
-                if(!Board.ba[i][j].getName().equals("   ")){
-                    decideLeftDown = false;
+            j--;
+                if(!Board.ba[i-1][j-1].getName().equals("   ")){
+                	count ++;
                 }
-            }
+            
+        }
+        if(count == 0){
+        	decideLeftDown = true;
         }
         return decideLeftDown;
+        
     }
 }
+
