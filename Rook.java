85a35dcaae706b4d2b03fa0b3c14164f7d1915b4
JayShu <18611525826@163.com>
2017-03-14 08:29:11 +0800

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -4,61 +4,114 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Rook implements Pieces
+public class Rook //implements Pieces
 {
     public String name;
     public int x,y;
     public String getName(){
         return name;
     }
+
     public void setX(int ix){
         x = ix;
     }
+
     public void setY(int iy){
         y = iy;
     }
+
     public Rook(int ix,int iy,String player){
         x = ix; y = iy;
         name = player+"Rk";
     }
+
     public int getX(){
         return x;
     }
+
     public int getY(){
         return y;
     }
-    public Boolean isAbleToMove(int tox,int toy){
+
+    public Boolean isAbleToMove(int toX,int toY){
         boolean decideMove = false;
-        if(getX()>tox&&getY()==toy) {
-            for(int i=getX();i<tox;i++) {
-                if(Board.ba[i][toy].getName().equals("   ")) {
-                    decideMove=true;
-                }
+        String direction = checkDirection(toX,toY);
+        if(checkStraightLine(toX,toY)) {
+            if(direction.equals("left")) {
+                decideMove = checkLeft(toX,toY);
             }
-        }
-        else if(getX()<tox&&getY()==toy) {
-            for(int i=tox;i<getX();i++) {
-                if(Board.ba[i][toy].getName().equals("   ")) {
-                    decideMove=true;
-                }
+            else if(direction.equals("right")) {
+                decideMove = checkRight(toX,toY);
             }
-        }
-        else if(getX()==tox&&getY()>toy) {
-            for(int i=toy;i<getY();i++) {
-                if(Board.ba[tox][i].getName().equals("   ")) {
-                    decideMove=true;
-                }
+            else if(direction.equals("down")) {
+                decideMove = checkDown(toX,toY);
             }
-        }
-        else if(getX()==tox&&getY()<toy) {
-            for(int i=getY();i<toy;i++) {
-                if(Board.ba[tox][i].getName().equals("   ")) {
-                    decideMove=true;
-                }
+            else if(direction.equals("up")) {
+                decideMove = checkUp(toX,toY);
             }
         }
         return decideMove;
     }
+
+    private boolean checkStraightLine(int toX, int toY) {
+        if(getX()==toX||getY()==toY) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private String checkDirection(int toX, int toY) {
+        if(getX()>toX&&getY()==toY) {
+            return "left";
+        }
+        else if(getX()<tox&&getY()==toy) {
+            return "right";
+        }
+        else if(getX()==tox&&getY()>toy) {
+            return "down";
+        }
+        else if(getX()==tox&&getY()<toy) {
+            return "up";
+        }
+    }
+
+    private boolean checkLeft(int toX, int toY) {
+        for(int i=toX;i<getX();i++) {
+            if(Board.ba[i][toY].getName().equals("   ")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkRight(int toX, int toY) {
+        for(int i=getX();i<toX;i++) {
+            if(Board.ba[i][toY].getName().equals("   ")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkDown(int toX, int toY) {
+        for(int i=toX;i<getX();i++) {
+            if(Board.ba[toX][i].getName().equals("   ")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkUp(int toX, int toY) {
+        for(int i=getY();i<toy;i++) {
+            if(Board.ba[tox][i].getName().equals("   ")) {
+                decideMove=true;
+            }
+        }
+    }
+
     public void move(int tox,int toy){
         x = tox;
         y = toy;
